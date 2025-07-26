798221f3e890a1559c2e1e919c9cf4b48c91632e
Harsh Phartiyal <harshphartiyal20@gmail.com>
2025-07-26 15:16:24 +0530

    Modified User file with the constructor, Getters & Setters....

--- student-management-system/src/main/java/com/student/management/model/User.java
+++ student-management-system/src/main/java/com/student/management/model/User.java
@@ -1,4 +1,67 @@
 package com.student.management.model;
 
 public class User {
+    private int id;
+    private String username;
+    private String passwordHash;
+    private String role;
+
+    //no-args constructor
+    public User() {
+    }
+
+    //args for new users
+    public User(String username, String passwordHash, String role) {
+        this.username = username;
+        this.passwordHash = passwordHash;
+        this.role = role;
+    }
+
+    //Getters & setters
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    //full-arguments constructor
+    public User(int id, String username, String passwordHash, String role) {
+        this.id = id;
+        this.username = username;
+        this.passwordHash = passwordHash;
+        this.role = role;
+    }
+
+    //Getters & Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    @Override
+    public String toString() {
+        return "User{id=" + id + ", username='" + username + '\'' +
+                ", role='" + role + '\'' + '}';
+    }
+
 }
